c32db4a1f846d80a87526e5d032e08e2578b3c21
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-02-03 11:59:55 +0700

    get apikey from .env file

--- geokoboard.py
+++ geokoboard.py
@@ -1,7 +1,14 @@
 import requests 
+import os
+from dotenv import load_dotenv
 
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+apikey = os.getenv('apikey')
+if not apikey:
+    print('can\'t get api key in env file')
 
-def testconnection(apikey):''' this method test requests to server '''
+def testconnection(apikey):
+    ''' this method test requests to server '''
     url = "https://api.geckoboard.com/"
     headers = {
         "Authorization": "Basic "+apikey
@@ -9,7 +16,7 @@ def testconnection(apikey):''' this method test requests to server '''
     response = requests.request("GET", url, headers=headers)
     return response.status_code
 
-result = testconnection('<you-api-key->')
+result = testconnection(apikey)
 print result
 
 # result status code if successfull it will return 200 otherwise error occur
