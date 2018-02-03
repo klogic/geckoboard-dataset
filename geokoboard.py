3450ea84f385a5e2e90a3ce51acf309907e40ca0
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-02-03 15:45:36 +0700

    append data into dataset

--- geokoboard.py
+++ geokoboard.py
@@ -49,7 +49,43 @@ payload = {
   "unique_by": ["timestamp"]
 }
 
-payload = json.dumps(payload)
-result = createdataset(apikey,'saleorder', payload)
-print result
+# payload = json.dumps(payload)
+# result = createdataset(apikey,'saleorder', payload)
+# print result
 # 201 status will return if dataset successful created
+
+def appenddata(apikey, id, payload):
+    ''' this method will append data into dataset '''
+    url = "https://api.geckoboard.com/datasets/" + id + "/data"
+    headers = {
+        "Authorization": "Basic "+apikey,
+        "Content-Type": "application/json"
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.status_code
+
+
+payload = {
+  "data": [
+    {
+      "timestamp": "2018-01-01T12:00:00Z",
+      "ordernumber": "th_00001",
+      "total": 819
+    },
+    {
+      "timestamp": "2018-01-02T12:00:00Z",
+      "ordernumber": "th_00002",
+      "total": 409
+    },
+    {
+      "timestamp": "2018-01-03T12:00:00Z",
+      "ordernumber": "th_00003",
+      "total": 164
+    }
+  ]
+}
+
+payload = json.dumps(payload)
+result = appenddata(apikey,'saleorder', payload)
+print result
+# 200 status will return if append data successful.
