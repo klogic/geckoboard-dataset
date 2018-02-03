0b8e51355d775f77d8c6ec06406b7894635c7e0c
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-02-03 13:54:44 +0700

    prepare and create dataset

--- geokoboard.py
+++ geokoboard.py
@@ -1,6 +1,7 @@
 import requests 
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 apikey = os.getenv('apikey')
@@ -16,7 +17,39 @@ def testconnection(apikey):
     response = requests.request("GET", url, headers=headers)
     return response.status_code
 
-result = testconnection(apikey)
-print result
-
+# result = testconnection(apikey)
+# print result
 # result status code if successfull it will return 200 otherwise error occur
+
+def createdataset(apikey, id, payload):
+    ''' this method will create dataset in geokoboard '''
+    url = "https://api.geckoboard.com/datasets/"+id
+    headers = {
+        "Authorization": "Basic "+apikey,
+        "Content-Type": "application/json"
+    }
+    response = requests.request("PUT", url, headers=headers, data=payload)
+    return response.status_code
+
+payload = {
+    "fields": {
+        "timestamp":{
+            "type": "datetime",
+            "name": "Date"
+        },
+        "string":{
+            "type": "string",
+            "name": "Order Number"
+        },
+        "amount":{
+            "type": "number",
+            "name": "Total"
+        }
+    },
+  "unique_by": ["timestamp"]
+}
+
+payload = json.dumps(payload)
+result = createdataset(apikey,'saleorder', payload)
+print result
+# 201 status will return if dataset successful created
